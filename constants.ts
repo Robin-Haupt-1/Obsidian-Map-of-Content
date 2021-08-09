0f1ce449bdfe2fc6e7b581dc038889c56b80fd8a
Robin-Haupt-1 <robinhaupt@outlook.com>
2021-08-09 20:08:06 +0200

    change arrow style

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 export const TLI_NAME="Top Level Index.md"
-export const LINKED_TO=" => "
-export const LINKED_FROM=" <= "
+export const LINKED_TO=" 🢡 "
+export const LINKED_FROM=" 🢠 "
 export const LINKED_TLI="None"
 export const LINKED_BOTH=" <=> "
 export const TLI_VIEW_TYPE = "tli index"
--- view.ts
+++ view.ts
@@ -23,7 +23,7 @@ export default class TLIView extends ItemView {
       console.log("displaying paths")
       if (all_paths){
         all_paths.forEach((p: path) => {
-        this.containerEl.createEl('h3', { text: lib.compilePath(p.items) })
+        this.containerEl.createEl('p', { text: lib.compilePath(p.items)})
 
       })
       }
