6cc8140b685302d92d2b1ac2af1a1976759476cb
Robin-Haupt-1 <85873542+Robin-Haupt-1@users.noreply.github.com>
2021-09-23 14:48:52 +0200

    replace arrows
    they dont render on ubuntu

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const TLI_NOTE_PATH_DEFAULT="Central Note.md"
-export const LINKED_TO=" 🢡 "
-export const LINKED_FROM=" 🢠 "
+export const LINKED_TO=" => "
+export const LINKED_FROM=" <= "
 export const LINKED_TLI="None"
 export const LINKED_BOTH=" <=> "
 export const TLI_VIEW_TYPE = "Automatic-MOC"
