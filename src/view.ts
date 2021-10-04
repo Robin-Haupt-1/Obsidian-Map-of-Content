e393b3fbf895dd56c171f693a6e583bfd096b4e2
Robin <85873542+Robin-Haupt-1@users.noreply.github.com>
2021-10-04 09:48:20 +0200

    show error message when no file has been opened

--- src/view.ts
+++ src/view.ts
@@ -2,23 +2,23 @@ import 'svelte'
 
 import { ItemView, Notice, TFile, WorkspaceLeaf } from 'obsidian'
 
+
 import { Log } from './utils';
 import { MOC_VIEW_TYPE } from './constants'
 import type MOCPlugin from './main'
 import type { Path } from './types'
 import type { DBManager } from './db'
+
 import Error from './svelte/Error.svelte'
 
 import PathView from './svelte/PathView.svelte'
-
-
 export default class MOCView extends ItemView {
   db: DBManager
   _app: PathView
-  errorview:Error
+  errorview: Error
   plugin: MOCPlugin
   open_file_path: string
- 
+
 
   constructor(leaf: WorkspaceLeaf) {
     super(leaf)
@@ -31,77 +31,87 @@ export default class MOCView extends ItemView {
 
     // update the path view every time a file is opened
     this.registerEvent(this.app.workspace.on("file-open", (file: TFile) => this.rerender()))
-    
+
     Log("Ppen path: " + this.app.workspace.getActiveFile().path, true)
 
     this.rerender()
   }
 
-  async onOpen(): Promise<void> { 
+  async onOpen(): Promise<void> {
 
   }
 
   /** reload paths and recreate the svelte instance */
   rerender(): void {
-    Log("Rerender called on view", true) 
+    Log("Rerender called on view", true)
 
     // destroy old pathview/errorview instance
     // set symbol to undefined to avoid "This component has already been destroyed" message
     if (this._app) {
       this._app.$destroy()
-      this._app=undefined
+      this._app = undefined
     }
+
     if (this.errorview) {
       this.errorview.$destroy()
-      this.errorview=undefined
+      this.errorview = undefined
     }
 
     // get path of open note
-    this.open_file_path = this.app.workspace.getActiveFile().path
 
-    let errors=[]
+
+    let errors = []
+    console.log(this.open_file_path)
 
     // make sure the database is usable
-    if (!this.db.database_complete){
+    if (!this.db.database_complete) {
       errors.push("Your vault could not be analyzed. Try updating the Map of Content again and make sure your Central Note exists")
     }
-    else if (this.db.getNoteFromPath(this.open_file_path) ==undefined){
-      // make sure file is in library   
-      errors.push("This note has not been indexed yet. Update the Map of Content")
+    // make sure a file is opened
+    let open_file = this.app.workspace.getActiveFile()
+    if (open_file == null) {
+      errors.push("No note has been opened")
+    }
+    else {
+      this.open_file_path = open_file.path
+      if (this.db.getNoteFromPath(this.open_file_path) == undefined) {
+        // make sure file is in library   
+        errors.push("This note has not been indexed yet. Update the Map of Content")
+      }
     }
 
     // Show error message if necessary
-    if (errors.length>0){
+    if (errors.length > 0) {
       this.errorview = new Error({
         target: (this as any).contentEl,
-        props: {message:errors[0]}, 
-        
+        props: { message: errors[0] },
+
       })
-      return 
-    } 
+      return
+    }
     // get paths to open note
     let all_paths = this.db.findPaths(this.open_file_path)
     if (all_paths.length == 0) {
       Log("No paths to this note", true)
-    } 
+    }
 
     // 
     let paths = all_paths.map((p: Path) =>
       p.items.slice()
     )
-    
+
     // create new pathview
     this._app = new PathView({
       target: (this as any).contentEl,
-      props: { paths: paths, app: this.app, db: this.db, cn_path: this.plugin.getCNPath(), open_note_path: this.open_file_path }, 
-      
-    }) 
+      props: { paths: paths, app: this.app, db: this.db, cn_path: this.plugin.getCNPath(), open_note_path: this.open_file_path },
+
+    })
   }
 
   getViewType(): string {
     return MOC_VIEW_TYPE
   }
-  
+
   getDisplayText(): string {
     return "Map of Content"
   }
